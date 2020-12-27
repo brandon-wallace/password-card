53fb0bb3eb3e50381959f127f714899c0cb3e5ac
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2020-12-26 22:38:53 -0500

    Add argparse

--- generate_card.py
+++ generate_card.py
@@ -2,31 +2,39 @@
 
 import string
 import random
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Create a password card.')
+parser.add_argument('--file', type=str, default='password_card.txt',
+                    required=False, help='Specify a file name.')
+args = parser.parse_args()
 
 
 def print_header(length):
-    print(' ', end='')
-    for a in range(length):
-        print(f' {"".join(string.ascii_lowercase)[a]}', end='')
-    print('\n')
+    '''Print header'''
+
+    return (f'  {" ".join(string.ascii_lowercase[:-7])}\n  {"-"*37}\n')
 
 
 def print_card():
+    '''Print password card'''
+
     row = 1
     length = 0
-    print_header(19)
-    chars = string.printable[:-6]
-    chars = list(chars)
-    random.shuffle(chars)
-    print(f'{row}', end='')
-    for a in chars:
-        length += 1
-
-        print(f' {a}', end='')
-        if length % 19 == 0:
-            print('\n')
-            row += 1
-            print(f'{row}', end='')
+    with open(args.file, 'w') as file:
+        file.write(str(print_header(19)))
+        chars = string.printable[:-6]
+        chars = list(chars)
+        random.shuffle(chars)
+        file.write(f'{row}')
+        for a in chars:
+            length += 1
+            file.write(f' {a}')
+            if length % 19 == 0:
+                file.write('\n\n')
+                row += 1
+                file.write(f'{row}')
 
 
 print_card()
