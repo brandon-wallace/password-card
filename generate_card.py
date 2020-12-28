cfa405c165b5c1b27a7dfd5593acb6863f3501b3
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2020-12-28 10:36:14 -0500

    Check existing file
    Added a function to check if file already exists.
    Prompt user to overwrite file or not.

--- generate_card.py
+++ generate_card.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
+'''
+
+Create a password card.
+
+Requirements:
+    Python3.6+
+
+'''
 
 import string
 import random
 import argparse
+from pathlib import Path
 
 
 parser = argparse.ArgumentParser(description='Create a password card.')
 parser.add_argument('--file', type=str, default='password_card.txt',
                     required=False, help='Specify a file name.')
 args = parser.parse_args()
+
+
+def overwrite_existing_file(file):
+    '''Check if file is existing'''
+
+    if Path(file).is_file():
+        overwrite = input('Overwrite existing password-card.txt file? (Yes or No) ')
+        if overwrite.lower().startswith('y'):
+            print(f'Overwriting {file} file!')
+        else:
+            args.file = input('Enter a filename: ')
+            print(f'Creating new file {args.file}!')
+    else:
+        return
 
 
 def print_header(length):
@@ -22,6 +45,7 @@ def print_card():
 
     row = 1
     length = 0
+    overwrite_existing_file(args.file)
     with open(args.file, 'w') as file:
         file.write(str(print_header(19)))
         chars = string.printable[:-6]
